add54ee79597a5a62a85edf4324ef53043cff7ef
Brendan <jbrendan70@outlook.com>
2019-02-04 20:51:44 +0000

    catch if server fails to start, only display graceful exist messsage on first sigint

--- index.js
+++ index.js
@@ -7,21 +7,23 @@ const col = require('./colourdat');
 //Quick console colours
 info = col.bg_blue+col.white
 request = col.bg_black+col.yellow
-warn = col.bg_red+col.white
+warn = col.bg_white+col.red
+errorc = col.bg_red+col.white
 success = col.bg_green+col.white
 none = col.reset+col.reset
 var exitVal = 0
 
 //Catch exit
 process.on('SIGINT', function() {
-    console.warn(warn+"[w] Exit requested - Press CTRL+C again to gracefully exit");
     if (exitVal) {
         console.warn(warn+"[w] Stopping server...")
         server.close()
+        console.warn(errorc+"[e] Server died")
         console.info(success+"[s] Server stopped")
         console.info(none+'Resetting terminal colour\n[i] Killing process')
         process.exit();
     } else {
+        console.warn(warn+"[w] Exit requested - Press CTRL+C again to gracefully exit");
         exitVal = 1
     }
 });
@@ -129,7 +131,16 @@ var server = http.createServer(function(req,res) {
 server.listen(config.port,config.ip,function(){
     console.log(success+'[s] Server is listening on ',col.inverse,config.ip+':'+config.port,none)
 })
-
+server.on('error',function(){
+    console.error(errorc+'[e] Failed to attach to the IP or port that was specified')
+    console.warn(warn+'[w] Exiting')
+    console.warn(warn+"[w] Stopping server...")
+    server.close()
+    console.warn(errorc+"[e] Server died")
+    console.info(success+"[s] Server stopped")
+    console.info(none+'Resetting terminal colour\n[i] Killing process')
+    process.exit();
+})
 
 
 /*
