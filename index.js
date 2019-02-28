b8874978c766b2528a8733a63f8a04a5c998cd51
Brendan <jbrendan70@outlook.com>
2019-02-28 20:46:58 +0000

    Data handler works, removing testing and going to merge

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-// Index file for the API.
+/*
+ * Main script for the API
+ * 
+ * Configuration is in config.js
+ * 
+ */
+
 
 // Console colours.
 const col = require('./lib/colours.js');
@@ -42,12 +48,8 @@ const http = require('http'),
     { StringDecoder } = require('string_decoder'),
     config = require('./config'),
     fs = require('fs'),
-    verCheck = require('./lib/versionChecker'),
-    data = require('./lib/dataHandler');
+    verCheck = require('./lib/versionChecker');
 
-data.create('test', 'demo', { a: 'a' }, function (err) {
-    log.error(err);
-});
 console.info(`\NJSAPIPROJ-${fs.readFileSync('.git/refs/heads/master').toString('utf-8')}\nUsing mode: ${config.env}\nhttps://github.com/dudeisbrendan03/RESTful-api\n`);
 if (config.ip === '0.0.0.0')
     log.warn('You are running on all available IPs. This is considered bad practice and possibly dangerous, make sure you have double checked your config.');
