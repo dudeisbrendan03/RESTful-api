5fd27c3529377e96f481d33fcba92ea00ae8ba3e
Brendan Jennings <jbrendan70@icloud.com>
2019-03-01 12:08:18 +0000

    Migrated logging into it's own script for future development.
    Going to introduce a log file in a few builds

--- index.js
+++ index.js
@@ -6,18 +6,13 @@
  */
 
 
-// Console colours.
-const col = require('./lib/colours.js');
+//Import logging
+const log = require('./lib/logging')
+//Import console colours
+const col = require('./lib/colours');
 let exitVal = 0;
 
-const log = {
-    info: text => console.info(`${col.background.blue + col.foreground.white}[i] ${text}${col.reset + col.reset}`),
-    error: text => console.warn(`${col.background.red + col.foreground.white}[e] ${text}${col.reset + col.reset}`),
-    success: text => console.info(`${col.background.green + col.foreground.white}[s] ${text}${col.reset + col.reset}`),
-    warn: text => console.warn(`${col.background.white + col.foreground.red}[w] ${text}${col.reset + col.reset}`),
-    request: text => console.log(`${col.background.black + col.foreground.yellow}[r] ${text}${col.reset + col.reset}`)
-};
-exports.log = log;
+
 
 // Catch exit
 process.on('SIGINT', () => {
