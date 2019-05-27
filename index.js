1f3c937eee2ab54df8bc26b8b85b685800d2a7ed
Brendan Jennings <jbrendan70@outlook.com>
2019-05-27 14:57:24 +0100

    Version markers

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ const http = require('http'),
 
 
 try {
-    console.info(`\NJSAPIPROJ-${fs.readFileSync('.git/refs/heads/master').toString('utf-8')}\nUsing mode: ${config.env}\nhttps://github.com/dudeisbrendan03/RESTful-api\n v0.2.230\n`);
+    console.info(`\NJSAPIPROJ-${fs.readFileSync('.git/refs/heads/master').toString('utf-8')}\nUsing mode: ${config.env}\nhttps://github.com/dudeisbrendan03/RESTful-api\n v0.2.233\n`);
 } catch (e) {
     console.error('Unknown version');
 }
