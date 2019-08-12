abc7514d85dcb940681611bf1071486ef066cd6c
Brendan Jennings <jbrendan70@outlook.com>
2019-08-12 19:49:34 +0100

    Shipping origin data🔖
    For people who build custom APIs from this, we have included origin data so users may lookup the version their API is based on and also investigate any security issues that have been raised since that version.
    
    This should *not* be removed and should be available at one of these uris "/version", "/instance/info" or "/info"

--- shipping.js
+++ shipping.js
@@ -16,4 +16,13 @@ shipping.version = {
     "release": false
 };
 
+//Do not remove this information, it is used to let users check the original version of the software so they can check for security vunerabilities that may expose their data. This allows users to protect themselves and their data. This is also used to credit ourselves (the creators). If you remove this and don't tell the useres you have modified the software on top of not disclosing the original creatores you will be breaking the license included in this project and we may legally pursue you.
+shipping.source = {
+    "version": "v0.3.DEV",
+    "release": false,
+    "git": "https://github.com/dudeisbrendan03/RESTful-api.git",
+    "url": "https://github.com/dudeisbrendan03/RESTful-api",
+    "latest-stable-version": "https://api.thatspretty.cool/instance/info"
+};
+
 module.exports = shipping;
